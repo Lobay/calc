860031486036c95d8655b27cda74ea8276db1716
Lobay <32384708+Lobay@users.noreply.github.com>
2017-10-12 18:40:39 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
-#include
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main (int argc, char*argv[]) {
+	char operand;
+	operand = argv [2][0];
+	if ((isdigit (argv [1][0])) && (isdigit (argv [3][0]))) {
+		switch (operand) {
+			case '+':
+				cout« atoi (argv[1]) + atoi (argv[3])« endl;
+				break;
+			case '-':
+				cout« atoi (argv[1]) - atoi (argv[3])« endl;
+				break;
+			default:
+				cerr« "error"« endl;
+		};
+	} else {
+		cerr« "error"« endl;
+	};
+}
